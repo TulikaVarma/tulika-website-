a9309206d5f82904b6f07d8add788f4a6a3876e6
Tulika Varma <tulikaavarma2003@gmail.com>
2026-02-27 22:05:27 -0800

    Click on the characters

--- src/components/ui/floating-amongus.jsx
+++ src/components/ui/floating-amongus.jsx
@@ -3,17 +3,25 @@ import { motion } from "framer-motion";
 import redAmongUs from "../../pictures/red.png";
 import greenAmongUs from "../../pictures/green.png";
 import blueAmongUs from "../../pictures/blue.png";
+import blueDeadAmongUs from "../../pictures/blue-dead.png"
+import orangeAmongUs from "../../pictures/orange.png";
+import orangeDeadAmongUs from "../../pictures/orange-dead.png";
+import cyanAmongUs from "../../pictures/cyan.png";
 
 export default function FloatingAmongUs() {
   const [amogusPos, setAmogusPos] = useState({ x: [0], y: [0], rotate: [0] });
   const [amogusPos2, setAmogusPos2] = useState({ x: [0], y: [0], rotate: [0] });
   const [amogusPos3, setAmogusPos3] = useState({ x: [0], y: [0], rotate: [0] });
+  const [amogusPos4, setAmogusPos4] = useState({ x: [0], y: [0], rotate: [0] });
+  const [amogusPos5, setAmogusPos5] = useState({ x: [0], y: [0], rotate: [0] });
+  const [orangeDead, setOrangeDead] = useState(false);
+  const [blueDead, setBlueDead] = useState(false);
 
   useEffect(() => {
     const generatePath = () => {
       const points = 12;
-      const w = window.innerWidth - 70;
-      const h = window.innerHeight - 70;
+      const w = window.innerWidth - 50;
+      const h = window.innerHeight - 50;
       const x = Array.from({ length: points }, () => Math.random() * w);
       const y = Array.from({ length: points }, () => Math.random() * h);
       const rotate = Array.from({ length: points }, () => Math.random() * 360 - 180);
@@ -26,6 +34,8 @@ export default function FloatingAmongUs() {
     setAmogusPos(generatePath());
     setAmogusPos2(generatePath());
     setAmogusPos3(generatePath());
+    setAmogusPos4(generatePath());
+    setAmogusPos5(generatePath());
   }, []);
 
   return (
@@ -51,10 +61,31 @@ export default function FloatingAmongUs() {
       <motion.div
         animate={{ x: amogusPos3.x, y: amogusPos3.y, rotate: amogusPos3.rotate }}
         transition={{ duration: 180, repeat: Infinity, ease: "easeInOut" }}
+        className="absolute"
+        style={{ top: 0, left: 0, zIndex: 2, pointerEvents: "auto", cursor: "pointer" }}
+        onClick={() => setBlueDead(true)}
+        >
+        <img src={blueDead ? blueDeadAmongUs : blueAmongUs} alt="" style={{ width: blueDead ? "60px" : "70px", height: "auto", opacity: 0.85 }} />
+      </motion.div>
+
+      {/* Orange - clickable */}
+      <motion.div
+        animate={{ x: amogusPos4.x, y: amogusPos4.y, rotate: amogusPos4.rotate }}
+        transition={{ duration: 220, repeat: Infinity, ease: "easeInOut" }}
+        className="absolute"
+        style={{ top: 0, left: 0, zIndex: 2, pointerEvents: "auto", cursor: "pointer" }}
+        onClick={() => setOrangeDead(true)}
+      >
+        <img src={orangeDead ? orangeDeadAmongUs : orangeAmongUs} alt="" style={{ width: orangeDead ? "60px" : "80px", height: "auto", opacity: 0.85 }} />
+      </motion.div>
+
+      <motion.div
+        animate={{ x: amogusPos5.x, y: amogusPos5.y, rotate: amogusPos5.rotate }}
+        transition={{ duration: 190, repeat: Infinity, ease: "easeInOut" }}
         className="absolute pointer-events-none"
         style={{ top: 0, left: 0, zIndex: 1 }}
       >
-        <img src={blueAmongUs} alt="" style={{ width: "70px", height: "auto", opacity: 0.85 }} />
+        <img src={cyanAmongUs} alt="" style={{ width: "85px", height: "auto", opacity: 0.85 }} />
       </motion.div>
     </div>
   );
